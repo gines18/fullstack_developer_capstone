7d2a679f2a0e2c598ae4ea99e2f215b889d35b37
gines18 <ruczkowski.m@gmail.com>
2024-04-25 12:14:27 -0400

    fix indentations

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
+
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
@@ -30,3 +31,5 @@ class CarModel(models.Model):
 
     def __str__(self):
         return self.name
+
+# Newline at the end of the file
