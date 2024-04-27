f65d4f7ae3f78dd37d964bce100585042ee6ce33
gines18 <ruczkowski.m@gmail.com>
2024-04-27 10:45:27 -0400

    add dockerfile, build, push and create app on ibmcloud

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -27,11 +27,15 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = 
-    ['localhost', 
-    'https://ruczkowskim-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS = 
-    ['https://ruczkowskim-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+ALLOWED_HOSTS = [
+    'localhost', 
+    'ruczkowskim-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
+    'dealership.1g9yquyni0b1.us-south.codeengine.appdomain.cloud'
+    ]
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://ruczkowskim-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    ]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
